f0a65209dd1801206652bbbd27ea33a90330e82b
Shashwot90 <shashwotpradhan@gmail.com>
2025-02-11 09:40:23 +0545

    fot

--- preprocess.py
+++ preprocess.py
@@ -10,4 +10,5 @@ def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"
     with open(raw_file_path, encoding='utf-8') as file:
         posts = json.load(file)
 
-        
+        for post in posts:
+            
