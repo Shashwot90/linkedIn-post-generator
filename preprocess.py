4aa9e7d29d34dea8fb444b2260d30fa88978b2f5
Shashwot90 <shashwotpradhan@gmail.com>
2025-02-11 09:40:07 +0545

    posts

--- preprocess.py
+++ preprocess.py
@@ -8,4 +8,6 @@ import unicodedata
 def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"):
     enriched_posts = []
     with open(raw_file_path, encoding='utf-8') as file:
+        posts = json.load(file)
+
         
