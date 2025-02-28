aaf4c181c833dcab2adce17ece778a08d2b4098d
Shashwot90 <shashwotpradhan@gmail.com>
2025-02-28 07:31:52 +0545

    comments

--- preprocess.py
+++ preprocess.py
@@ -59,4 +59,6 @@ def extract_metadata(post):
     try:
         json_parser = JsonOutputParser()
         res = json_parser.parse(response.content)
+    # except OutputParserException:
+    #     raise OutputParserException("Context too big. Unable to parse jobs.")
     
