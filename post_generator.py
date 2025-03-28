cd3f446b993a2a7c8fef4635cc55fbe0752ec9cc
Shashwot90 <shashwotpradhan@gmail.com>
2025-03-28 06:28:24 +0545

    comment

--- post_generator.py
+++ post_generator.py
@@ -31,4 +31,7 @@ def get_prompt(length, language, tag):
     3) Language: {language}
     If Language is Hinglish then it means it is a mix of Hindi and English. 
     The script for the generated post should always be English.
+    '''
+    # prompt = prompt.format(post_topic=tag, post_length=length_str, post_language=language)
+
     
