55027b923fd3108208cabb4d22737c425491429c
Shashwot90 <shashwotpradhan@gmail.com>
2025-04-05 06:32:01 +0545

    if

--- llm_helper.py
+++ llm_helper.py
@@ -7,5 +7,3 @@ llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.2-90b
 
 
 if __name__ == "__main__":
-    response = llm.invoke("Two most important ingradient in samosa are ")
-    print(response.content)
