15a5e3240df9d7f846dbaf60437a0bef9503ddb2
Shashwot90 <shashwotpradhan@gmail.com>
2025-04-10 05:36:40 +0545

    self

--- few_shot.py
+++ few_shot.py
@@ -7,4 +7,7 @@ class FewShotPosts:
     def __init__(self, file_path="data/processed_posts.json"):
         self.df = None
         self.unique_tags = None
+        self.load_posts(file_path)
+
+    def load_posts(self, file_path):
         
