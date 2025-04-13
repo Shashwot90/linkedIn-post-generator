51ce8e995b2342e23e4e52ff7a3baff2c00751c0
Shashwot90 <shashwotpradhan@gmail.com>
2025-04-13 06:42:15 +0545

    unique tags

--- few_shot.py
+++ few_shot.py
@@ -16,4 +16,6 @@ class FewShotPosts:
             self.df['length'] = self.df['line_count'].apply(self.categorize_length)
             # collect unique tags
             all_tags = self.df['tags'].apply(lambda x: x).sum()
-            
+            self.unique_tags = list(set(all_tags))
+
+    
