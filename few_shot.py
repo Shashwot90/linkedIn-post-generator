b89e5f21f3a1428c74c65c823840ea0dcd240569
Shashwot90 <shashwotpradhan@gmail.com>
2025-04-14 06:17:15 +0545

    self

--- few_shot.py
+++ few_shot.py
@@ -20,4 +20,5 @@ class FewShotPosts:
 
     def get_filtered_posts(self, length, language, tag):
         df_filtered = self.df[
+            (self.df['tags'].apply(lambda tags: tag in tags)) &  # Tags contain 'Influencer'
             
